d91d2568124beb88a866a05e53b8b68ae1f320d7
anishsuvarna11 <71928805+anishsuvarna11@users.noreply.github.com>
2022-11-27 23:57:37 -0500

    final

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -14,12 +14,12 @@ export default function Navbar(props) {
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
               to="/"
-              className="text-blueGray-400 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              className="text-blueGray-400 text-sm font-bold leading-relaxed inline-block mr-4 py-2 bg-black uppercase"
             >
               Launch
             </Link>
             <button
-              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              className="cursor-pointer text-xl leading-none px-3 py-1  border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
@@ -28,7 +28,7 @@ export default function Navbar(props) {
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+              "lg:flex flex-grow items-center bg-black" +
               (navbarOpen ? " block" : " hidden")
             }
             id="example-navbar-warning"
@@ -40,7 +40,7 @@ export default function Navbar(props) {
 
               <Link
               to="/Profile"
-              className="text-red-500 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap"
+              className="text-red-500 text-sm font-bold leading-relaxed inline-block mr-4 py-2 bg-black"
             >
               Apply
             </Link>
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
+import React from "react";
 
 const Profile = () => {
   const [input, setInput] = useState('')
@@ -35,63 +36,32 @@ const Profile = () => {
   return (
     <>
     <IndexNavbar fixed />
-    <section className=" bg-black header relative pt-16 items-center flex h-screen max-h-860-px">
-      <div className="z-10 container mx-auto items-center flex flex-wrap">
-        <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
-          <div className="pt-32 sm:pt-0">
-          <h2 className="font-semibold text-4xl text-white">
-              Complete the form today
-            </h2>
-            <p className="mt-4 text-lg leading-relaxed text-white">
-            </p>
-
-          </div>
-        </div>
-      </div>
-
-      <img
-        className="absolute z-20 top-0 b-auto right-0 pt-16"
-        src={require("assets/img/kkkk.gif").default}
-        alt="..."
-      />
-    </section>
-    
-    <div className='p-8 justify-center items-center h-screen flex'>
-
-<form name="contact" method="POST" netlify>
-  <div className = "flex flex-col">
-        <input required name="email" type="email" placeholder="Enter your email..." />
-        <label for="first">First name:</label>
-  <input type="text" id="first" name="first" />
-  <label for="last">Last name:</label>
-  <input type="text" id="last" name="last" />
-        <button type="submit">Subscribe for Updates</button>
-        </div>
-      </form>
-    </div>
-    <form class="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500">
-      <h1 class="text-2xl font-bold dark:text-gray-50">Send a message</h1>
-
-      <label for="fullname" class="text-gray-500 font-light mt-8 dark:text-gray-50">Full name<span class="text-red-500 dark:text-gray-50">*</span></label>
-      <input type="text" name="fullname" class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" />
-
-      <label for="email" class="text-gray-500 font-light mt-4 dark:text-gray-50">E-mail<span class="text-red-500">*</span></label>
-      <input type="email" name="email" class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" />
-
-      <label for="subject" class="text-gray-500 font-light mt-4 dark:text-gray-50">Subject<span class="text-red-500">*</span></label>
-      <input type="text" name="subject" class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500" />
-
-      <label for="message" class="text-gray-500 font-light mt-4 dark:text-gray-50">Message<span class="text-red-500">*</span></label>
-      <textarea name="message" class="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"></textarea>
-      <div class="flex flex-row items-center justify-start">
-        <button class="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center">
-          Send
-          <svg width="24" height="24" viewBox="0 0 24 24" class="text-cyan-500 ml-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z" fill="currentColor" />
-          </svg>
-        </button>
-      </div>
-    </form>
+    <div className = "bg-black h-fit items-center pl-12 pb-32">
+<div className="flex bg-slate-100 p-8 justify-center">
+  <div className='pt-24 text-white text-3xl'>1. Make a copy of the PDF file below and fill in all of the information.</div>
+</div>
+<div className="flex bg-slate-100 p-8 justify-center">
+  <div className='pt-4 text-white text-3xl'>    2. Download your completed file and send it to ___ as an attachment.</div>
+</div>
+<div className="flex bg-slate-100 p-8 justify-center">
+  <div className='pt-4 text-white text-3xl'>    3. Sit tight as the opportunities start to roll by.</div>
+</div>
+<div className="pt-16 research-pdf flex justify-center">
+                        <h1 className="flex items-center uppercase tracking-wide text-center text-xl mb-5 text-indigo-500 font-extralight semibold">
+                            <span className=" inline text-lg leading-tight font-extralight semibold text-red-400">
+                                {" "}
+                            </span>
+                        </h1>
+                        <iframe
+                            frameBorder="0"
+                            scrolling="no"
+                            width="600"
+                            height="650"
+                            src="https://drive.google.com/file/d/1ccXb22ZmYMy6kTJxPdasaUCQXSfMxM7t/preview"
+                        />
+                    </div>
+</div>
+<div className = "bg-black"></div>
     </>
   )
 }
